2e69ed7ba632b211e0fc59f0c69f04b6ba85cc3f
Aleksandr Pogorelov <aleksandrpogorelov@Admins-Mac-mini-95.local>
2017-04-07 08:47:50 +0300

    add constructor

--- js/main.js
+++ js/main.js
@@ -1,97 +1,136 @@
+function MyPlayer(src) {
+    const myAudio = new Audio();
 
-const myAudio = new Audio();
-console.log(myAudio.prototype);
-myAudio.addEventListener('timeupdate', currentTimeUpdate);
-
-myAudio.addEventListener('abort', function () {
-    let currentLine = document.querySelector('.currentLine');
-    currentLine.style.width = 0;
-});
-
-function startPlay () {
-    const list = [].slice.call(document.querySelectorAll('.list'));
-    const activeClass = list.filter(item => item.classList.contains('active'));
-    if(!activeClass.length) {
-        list[0].click();
-    } else {
-        myAudio.play();
-        addPauseButton();
-        myAudio.addEventListener('timeupdate', currentTimeUpdate);
-    }
-}
-
-function stopPlay () {
-    addPlayButton();
-    myAudio.pause();
-}
-
-myAudio.addEventListener('loadedmetadata', function () {
-    const endPlayTime = document.querySelector('.end');
-    const allDuration = Math.floor(myAudio.duration);
-    const minutes = Math.floor(allDuration/60);
-    const seconds = allDuration - minutes*60;
-    endPlayTime.innerHTML = (minutes + ':' + seconds);
-});
-
-function moveTime (xPos) {
-    let linePos = document.querySelector('.playLine');
-    let currentLine = document.querySelector('.currentLine');
-    let toTime = (xPos - linePos.getBoundingClientRect().left)/330*100;
-    currentLine.style.width = toTime + '%';
-    myAudio.removeEventListener('timeupdate', currentTimeUpdate);
-
-    linePos.onmousemove = function (e) {
-        toTime = (e.clientX - linePos.getBoundingClientRect().left)/330*100;
-        currentLine.style.width = toTime + '%';
-        const currentPlayTime = document.querySelector('.begin');
-        const time = Math.round(toTime*Math.round(myAudio.duration)/100);
-        currentPlayTime.innerHTML = (getMinuteSecond(time));
-    };
-
-    document.onmouseup = function () {
-        linePos.onmousemove = null;
-        document.onmouseup = null;
-        myAudio.addEventListener('timeupdate', currentTimeUpdate);
-        myAudio.currentTime = (toTime*Math.round(myAudio.duration)/100);
-    };
-}
-
-function setTrackFromList (src) {
-    myAudio.src = src;
-    myAudio.addEventListener('durationchange', startPlay);
-    myAudio.addEventListener('ended', forward);
-}
-
-
-function forward () {
-    const list = [].slice.call(document.querySelectorAll('.list'));
-    const activeClass = list.filter( item => item.classList.contains('active'));
-    if(activeClass.length) {
-        if(activeClass[0].nextElementSibling) {
-            activeClass[0].nextElementSibling.click();
-        } else {
+    function startPlay () {
+        const list = [].slice.call(document.querySelectorAll('.list'));
+        const activeClass = list.filter(item => item.classList.contains('active'));
+        if(!activeClass.length) {
             list[0].click();
+        } else {
+            myAudio.play();
+            addPauseButton();
+            myAudio.addEventListener('timeupdate', currentTimeUpdate);
         }
-    } else {
-        list[0].click();
     }
-}
 
-function backward () {
-    const list = [].slice.call(document.querySelectorAll('.list'));
-    if(myAudio.currentTime < 10) {
-        let activeClass = list.filter( item => {return item.classList.contains('active')});
+    function stopPlay () {
+        addPlayButton();
+        myAudio.pause();
+    }
+
+    function moveTime (xPos) {
+        let linePos = document.querySelector('.playLine');
+        let currentLine = document.querySelector('.currentLine');
+        let toTime = (xPos - linePos.getBoundingClientRect().left)/330*100;
+        currentLine.style.width = toTime + '%';
+        myAudio.removeEventListener('timeupdate', currentTimeUpdate);
+
+        linePos.onmousemove = function (e) {
+            toTime = (e.clientX - linePos.getBoundingClientRect().left)/330*100;
+            currentLine.style.width = toTime + '%';
+            const currentPlayTime = document.querySelector('.begin');
+            const time = Math.round(toTime*Math.round(myAudio.duration)/100);
+            currentPlayTime.innerHTML = (getMinuteSecond(time));
+        };
+
+        document.onmouseup = function () {
+            linePos.onmousemove = null;
+            document.onmouseup = null;
+            myAudio.addEventListener('timeupdate', currentTimeUpdate);
+            myAudio.currentTime = (toTime*Math.round(myAudio.duration)/100);
+        };
+    }
+
+    function forward () {
+        const list = [].slice.call(document.querySelectorAll('.list'));
+        const activeClass = list.filter( item => item.classList.contains('active'));
         if(activeClass.length) {
-            if(activeClass[0].previousElementSibling) {
-                activeClass[0].previousElementSibling.click();
+            if(activeClass[0].nextElementSibling) {
+                activeClass[0].nextElementSibling.click();
             } else {
-                list[list.length-1].click();
+                list[0].click();
             }
         } else {
             list[0].click();
         }
-    } else {
-        myAudio.currentTime = 0;
     }
 
+    function backward () {
+        const list = [].slice.call(document.querySelectorAll('.list'));
+        if(myAudio.currentTime < 10) {
+            let activeClass = list.filter( item => {return item.classList.contains('active')});
+            if(activeClass.length) {
+                if(activeClass[0].previousElementSibling) {
+                    activeClass[0].previousElementSibling.click();
+                } else {
+                    list[list.length-1].click();
+                }
+            } else {
+                list[0].click();
+            }
+        } else {
+            myAudio.currentTime = 0;
+        }
+
+    }
+
+    function currentTimeUpdate () {
+        const currPlayTime = myAudio.currentTime.toFixed(0);
+        const currentPlayTime = document.querySelector('.begin');
+        currentPlayTime.innerHTML = (getMinuteSecond(currPlayTime));
+
+        const durationPercent = currPlayTime/Math.round(myAudio.duration)*100;
+        const timeValue = document.querySelector('.currentLine');
+        timeValue.style.width = durationPercent+'%';
+    }
+
+    function abort() {
+        const endPlayTime = document.querySelector('.end');
+        const allDuration = Math.floor(myAudio.duration);
+        const minutes = Math.floor(allDuration/60);
+        const seconds = allDuration - minutes*60;
+        endPlayTime.innerHTML = (minutes + ':' + seconds);
+    }
+
+    function addPlayButton () {
+        const playButton = document.querySelector('.playPause');
+        playButton.src = 'img/white-play-button.svg';
+        playButton.onclick = () => startPlay();
+    }
+
+    function addPauseButton () {
+        const playButton = document.querySelector('.playPause');
+        playButton.src = 'img/pause-sign.svg';
+        playButton.onclick = () => stopPlay();
+    }
+
+    function getMinuteSecond (time) {
+        const minutes = Math.floor(time/60);
+        const seconds = time - minutes*60;
+        const viewSeconds = seconds < 10 ? '0' + seconds : seconds;
+        return minutes + ':' + viewSeconds;
+    }
+
+    this.init = function () {
+        myAudio.addEventListener('timeupdate', currentTimeUpdate);
+        myAudio.addEventListener('abort', function () {
+            let currentLine = document.querySelector('.currentLine');
+            currentLine.style.width = 0;
+        });
+        myAudio.addEventListener('loadedmetadata', abort);
+        document.querySelector('.playPause').onclick = () => startPlay();
+        document.querySelector('.forward').onclick = () => forward();
+        document.querySelector('.back').onclick = () => backward();
+        document.querySelector('.playLine').onmousedown = (event) => moveTime(event.clientX);
+    };
+
+    this.setTrackFromList = function (src) {
+        myAudio.src = src;
+        myAudio.addEventListener('durationchange', startPlay);
+        myAudio.addEventListener('ended', forward);
+    };
+
 }
+
+const player = new MyPlayer();
+player.init();
--- js/visualPart.js
+++ js/visualPart.js
@@ -11,72 +11,45 @@ trackList.forEach(({src, votes, img}) => {
     }, {
         tags: ["title","artist","album","picture"]
     });
+
+    function showTags(url, img, li) {
+        const tags = ID3.getAllTags(url);
+        console.log(tags);
+        let { title, artist, album } = tags;
+        addTitleAndArtist(li, title, artist, album);
+        addGif(li);
+        li.onclick = (evt) => {
+            player.setTrackFromList(url);
+            setTags(evt.target, title, artist, img);
+        };
+
+        ulTrackList.appendChild(li);
+    }
+
+    function setTags (target, title, artist, img) {
+        document.querySelector('.playingTitle').textContent = title || "Undefined";
+        document.querySelector('.playingAuthor').textContent = artist || "Unnkown";
+        document.querySelector('.imgAlbumPicture').src = img;
+        document.querySelectorAll('.list').forEach(item => item.classList.remove('active'));
+        target.classList.add('active');
+    }
+
+    function addTitleAndArtist(li, title, artist, album) {
+        const titleAndAuthor = document.createElement('div');
+        titleAndAuthor.classList.add('titleAndAuthor');
+        titleAndAuthor.innerHTML = `<div class="rotatePart"><span>${title}</span><br/><span>${artist}</span></div><div class="backRotatePart">${album}</div>`;
+        li.appendChild(titleAndAuthor);
+    }
+
+    function addGif (li) {
+        const div = document.createElement('div');
+        div.classList.add('gifDiv');
+        const imgGif = document.createElement('img');
+        imgGif.classList.add('gifImg');
+        imgGif.src = 'img/gif_sound_color.gif';
+        div.appendChild(imgGif);
+        li.appendChild(div);
+    }
 });
 
-function showTags(url, img, li) {
-    const tags = ID3.getAllTags(url);
-    console.log(tags);
-    let { title, artist, album } = tags;
-    addTitleAndArtist(li, title, artist, album);
-    addGif(li);
-    li.onclick = (evt) => {
-                setTrackFromList(url);
-                setTags(evt.target, title, artist, img);
-                };
 
-    ulTrackList.appendChild(li);
-}
-
-function currentTimeUpdate () {
-    const currPlayTime = myAudio.currentTime.toFixed(0);
-    const currentPlayTime = document.querySelector('.begin');
-    currentPlayTime.innerHTML = (getMinuteSecond(currPlayTime));
-
-    const durationPercent = currPlayTime/Math.round(myAudio.duration)*100;
-    const timeValue = document.querySelector('.currentLine');
-    timeValue.style.width = durationPercent+'%';
-}
-
-function getMinuteSecond (time) {
-    const minutes = Math.floor(time/60);
-    const seconds = time - minutes*60;
-    const viewSeconds = seconds < 10 ? '0' + seconds : seconds;
-    return minutes + ':' + viewSeconds;
-}
-
-function addPlayButton () {
-    const playButton = document.querySelector('.playPause');
-    playButton.src = 'img/white-play-button.svg';
-    playButton.onclick = () => startPlay();
-}
-
-function addPauseButton () {
-    const playButton = document.querySelector('.playPause');
-    playButton.src = 'img/pause-sign.svg';
-    playButton.onclick = () => stopPlay();
-}
-
-function setTags (target, title, artist, img) {
-    document.querySelector('.playingTitle').textContent = title || "Undefined";
-    document.querySelector('.playingAuthor').textContent = artist || "Unnkown";
-    document.querySelector('.imgAlbumPicture').src = img;
-    document.querySelectorAll('.list').forEach(item => item.classList.remove('active'));
-    target.classList.add('active');
-}
-
-function addTitleAndArtist(li, title, artist, album) {
-    const titleAndAuthor = document.createElement('div');
-    titleAndAuthor.classList.add('titleAndAuthor');
-    titleAndAuthor.innerHTML = `<div class="rotatePart"><span>${title}</span><br/><span>${artist}</span></div><div class="backRotatePart">${album}</div>`;
-    li.appendChild(titleAndAuthor);
-}
-
-function addGif (li) {
-    const div = document.createElement('div');
-    div.classList.add('gifDiv');
-    const imgGif = document.createElement('img');
-    imgGif.classList.add('gifImg');
-    imgGif.src = 'img/gif_sound_color.gif';
-    div.appendChild(imgGif);
-    li.appendChild(div);
-}
