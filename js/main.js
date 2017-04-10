b2ec2866f1aa2e10fa460ab300ab238fe8f6480d
Aleksandr Pogorelov <aleksandrpogorelov@Admins-Mac-mini-95.local>
2017-04-10 17:42:30 +0300

    april

--- js/main.js
+++ js/main.js
@@ -2,67 +2,64 @@ function MyPlayer() {
     const myAudio = new Audio();
 
     function startPlay () {
-        const list = getTrackList();
+        const list = util.getTrackList();
         const activeClass = list.filter(item => item.classList.contains('active'));
         if(!activeClass.length) {
             list[0].click();
         } else {
-            addPlaying();
+            util.addPlaying();
             myAudio.play();
-            myAudio.addEventListener('timeupdate', () => currentTimeUpdate(myAudio));
+            myAudio.addEventListener('timeupdate', () => util.currentTimeUpdate(myAudio));
         }
     }
 
     function stopPlay () {
-        removePlaying();
+        util.removePlaying();
         myAudio.pause();
     }
 
     function moveTime (xPos) {
 
-        const { linePos, currentLine } = setLinePosition();
+        const { linePos, currentLine } = util.setLinePosition();
 
         let toTime = (xPos - linePos.getBoundingClientRect().left)/330*100;
         currentLine.style.width = toTime + '%';
-        myAudio.removeEventListener('timeupdate', () => currentTimeUpdate(myAudio));
+        myAudio.removeEventListener('timeupdate', () => util.currentTimeUpdate(myAudio));
 
         linePos.onmousemove = function (e) {
             toTime = (e.clientX - linePos.getBoundingClientRect().left)/330*100;
             currentLine.style.width = toTime + '%';
-            setBeginTime(toTime);
+            util.setBeginTime(toTime);
         };
 
         document.onmouseup = function () {
             linePos.onmousemove = null;
             document.onmouseup = null;
-            myAudio.addEventListener('timeupdate', () => currentTimeUpdate(myAudio));
+            myAudio.addEventListener('timeupdate', () => util.currentTimeUpdate(myAudio));
             myAudio.currentTime = (toTime*Math.round(myAudio.duration)/100);
         };
     }
 
     function forward () {
-        const list = getTrackList();
+        const list = util.getTrackList();
         const activeClass = list.filter( item => item.classList.contains('active'));
 
         if(activeClass.length) {
-
             if(activeClass[0].nextElementSibling) {
                 activeClass[0].nextElementSibling.click();
             } else {
                 list[0].click();
             }
-
         } else {
             list[0].click();
         }
     }
 
     function backward () {
-        const list = getTrackList();
+        const list = util.getTrackList();
 
         if(myAudio.currentTime < 10) {
             let activeClass = list.filter( item => {return item.classList.contains('active')});
-
             if(activeClass.length) {
                 if(activeClass[0].previousElementSibling) {
                     activeClass[0].previousElementSibling.click();
@@ -72,19 +69,18 @@ function MyPlayer() {
             } else {
                 list[0].click();
             }
-
         } else {
             myAudio.currentTime = 0;
         }
     }
 
     this.init = function () {
-        myAudio.addEventListener('timeupdate', () => currentTimeUpdate(myAudio));
+        myAudio.addEventListener('timeupdate', () => util.currentTimeUpdate(myAudio));
         myAudio.addEventListener('abort', function () {
             const currentLine = document.querySelector('.currentLine');
             currentLine.style.width = 0;
         });
-        myAudio.addEventListener('loadedmetadata', () => endTime(myAudio));
+        myAudio.addEventListener('loadedmetadata', () => util.endTime(myAudio));
         document.querySelector('.playPause').onclick = () => startPlay();
         document.querySelector('.pausePlay').onclick = () => stopPlay();
         document.querySelector('.forward').onclick = () => forward();
--- js/utils.js
+++ js/utils.js
@@ -1,52 +1,56 @@
+function Utilities () {
 
-function currentTimeUpdate (myAudio) {
-    const currPlayTime = myAudio.currentTime.toFixed(0);
-    const currentPlayTime = document.querySelector('.begin');
-    currentPlayTime.innerHTML = (getMinuteSecond(currPlayTime));
-
-    const durationPercent = currPlayTime/Math.round(myAudio.duration)*100;
-    const timeValue = document.querySelector('.currentLine');
-    timeValue.style.width = durationPercent+'%';
-}
-
-function endTime (myAudio) {
-    const endPlayTime = document.querySelector('.end');
-    const allDuration = Math.floor(myAudio.duration);
-    const minutes = Math.floor(allDuration/60);
-    const seconds = allDuration - minutes*60;
-    endPlayTime.innerHTML = (minutes + ':' + seconds);
-}
-
-function getTrackList () {
-    const list = document.querySelectorAll('.list');
-    return [].slice.call(list)
-}
-
-function addPlaying () {
     const playButton = document.querySelector('.playPauseBorder');
-    playButton.classList.add('playing');
+
+    this.currentTimeUpdate= function (myAudio) {
+        const currPlayTime = myAudio.currentTime.toFixed(0);
+        const currentPlayTime = document.querySelector('.begin');
+        currentPlayTime.innerHTML = (getMinuteSecond(currPlayTime));
+
+        const durationPercent = currPlayTime/Math.round(myAudio.duration)*100;
+        const timeValue = document.querySelector('.currentLine');
+        timeValue.style.width = durationPercent+'%';
+    };
+
+    this.endTime = function (myAudio) {
+        const endPlayTime = document.querySelector('.end');
+        const allDuration = Math.floor(myAudio.duration);
+        const minutes = Math.floor(allDuration/60);
+        const seconds = allDuration - minutes*60;
+        endPlayTime.innerHTML = (minutes + ':' + seconds);
+    };
+
+    this.getTrackList = function () {
+        const list = document.querySelectorAll('.list');
+        return [].slice.call(list)
+    };
+
+    this.addPlaying = function () {
+        playButton.classList.add('playing');
+    };
+
+    this.removePlaying = function () {
+        playButton.classList.remove('playing');
+    };
+
+    this.setLinePosition = function () {
+        const linePos = document.querySelector('.playLine');
+        const currentLine = linePos.querySelector('.currentLine');
+        return {linePos, currentLine};
+    };
+
+    this.setBeginTime = function (toTime) {
+        const currentPlayTime = document.querySelector('.begin');
+        const time = Math.round(toTime*Math.round(myAudio.duration)/100);
+        currentPlayTime.innerHTML = (getMinuteSecond(time));
+    };
+
+    function getMinuteSecond (time) {
+        const minutes = Math.floor(time/60);
+        const seconds = time - minutes*60;
+        const viewSeconds = seconds < 10 ? '0' + seconds : seconds;
+        return minutes + ':' + viewSeconds;
+    }
 }
 
-function removePlaying () {
-    const playButton = document.querySelector('.playPauseBorder');
-    playButton.classList.remove('playing');
-}
-
-function setLinePosition () {
-    const linePos = document.querySelector('.playLine');
-    const currentLine = linePos.querySelector('.currentLine');
-    return {linePos, currentLine};
-}
-
-function setBeginTime (toTime) {
-    const currentPlayTime = document.querySelector('.begin');
-    const time = Math.round(toTime*Math.round(myAudio.duration)/100);
-    currentPlayTime.innerHTML = (getMinuteSecond(time));
-}
-
-function getMinuteSecond (time) {
-    const minutes = Math.floor(time/60);
-    const seconds = time - minutes*60;
-    const viewSeconds = seconds < 10 ? '0' + seconds : seconds;
-    return minutes + ':' + viewSeconds;
-}
+const util = new Utilities();
